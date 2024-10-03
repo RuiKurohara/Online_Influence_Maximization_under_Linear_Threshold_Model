5e8bc50f5b50dbac70ff23d517cae6dcdaf6d71f
Rui Kurohara <968ride@gmail.com>
2024-10-03 11:37:50 +0900

    pow=1

--- run_with_limit copy.ps1
+++ run_with_limit copy.ps1
@@ -1,21 +1,31 @@
 # run_with_limit.ps1
 
 # 実行回数を指定
-$max_runs = 2  # 実行回数を変更できます
+$max_runs = 5  # 実行回数を変更できます
 $wait_time = 5 # 次の実行までの待機時間（秒単位）
-
-for ($i = 1; $i -le $max_runs; $i++) {
-    Write-Host "Run #$i"
-    
-    # Pythonスクリプトを実行
-    python Main.py --seed_size 5 --iterationTimes 10 --save_address SimulationResults/instagram_celfpp_mc5_ER --G_address Datasets/Instagram.G --weight_address Datasets/Instagram_EWTrue.dic --use_new_algorithm
-    
-    Write-Host "Run #$i finished."
-    
-    if ($i -lt $max_runs) {
-        Write-Host "Waiting for $wait_time seconds before the next run..."
-        Start-Sleep -Seconds $wait_time
+#$seed_size = 3 #シードサイズ
+#$iteration = 10000 #反復数
+$node_num = 25 #ノード数
+$iterations = @(1000,5000,10000) #反復数
+$seed_sizes = @(3,5) #ノード数
+foreach($seed_size in $seed_sizes){
+    foreach($iteration in $iterations){
+        for ($i = 1; $i -le $max_runs; $i++) {
+            Write-Host "Run #$i"
+            
+            # Pythonスクリプトを実行
+            $command = "python Main.py --seed_size $seed_size --iterationTimes $iteration --save_address SimulationResults/AETC_2/gaussian_${node_num}_AETC_${iteration}_k${seed_size}_ER --G_address Datasets/ER_node${node_num}_p_0.2.G --weight_address Datasets/ER_node${node_num}_p_0.2EWTrue.dic"
+            
+            Invoke-Expression $command
+            Write-Host "Run #$i finished."
+            
+            if ($i -lt $max_runs) {
+                Write-Host "Waiting for $wait_time seconds before the next run..."
+                Start-Sleep -Seconds $wait_time
+            }
+        }
     }
 }
-
 Write-Host "All $max_runs runs completed."
+
+#.\run_with_limit.ps1
